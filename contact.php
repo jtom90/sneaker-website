e785049489f8d16e1fe1a46d052223c046a8c142
Jeffrey Tom <jeffrey.tom90@gmail.com>
2016-04-04 23:42:09 -0400

    Updating gh-pages from master branch via GitBot

---
+++ contact.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h1>Contact Form</h1>	
+
+<form action="welcome.php" method="post">
+
+Name: <input type="text" name="fullname">
+
+E-mail: <input type="text" name="email">
+
+Message: <textarea name="message" cols="30" rows="20"></textarea>
+<input type="submit">
+</form>
+
+</body>
+</html>
